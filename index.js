4ee6699c92536ff62f59eb6b125c8d9e27eb1739
Justin Feldmann <jfeld962@charter.net>
2019-12-26 16:47:12 -0600

    Added some stewf

---
+++ index.js
@@ -0,0 +1,14 @@
+var jumbotron = new Vue({
+    el: '#app',
+    data: {
+        message: 'Justin Feldmann',
+        emailBtn: "Hire Me!"
+    }
+})
+
+var emailBtn = new Vue({
+    el: '#hire-me',
+    data: {
+
+    }
+})
